0b5638e5f0399abb9cc84db657e5800fc7bfdcad
AlexaJasso <alexajassoj@gmail.com>
2020-06-20 18:24:42 +0000

    Almost done with duck.

---
+++ src/_04_popcorn/PopcornRunner.java
@@ -0,0 +1,21 @@
+package _04_popcorn;
+
+import javax.swing.JOptionPane;
+
+public class PopcornRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String flavor = JOptionPane.showInputDialog(" What flavor do you want for your popcorn? ");
+Popcorn corn = new Popcorn(flavor);
+Microwave pop = new Microwave();
+
+String time = JOptionPane.showInputDialog(" How long do you want your popcorn in the microwave for? ");
+int minutes= Integer.parseInt(time);
+pop.putInMicrowave(corn);
+pop.startMicrowave();
+	
+	
+	}
+
+}
