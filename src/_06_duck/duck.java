754d5f726cb587c368cae406f2f93d16b0f0e6ae
root <root@fba1c4195f9b>
2020-09-05 18:24:05 +0000

    Started on duck.

---
+++ src/_06_duck/duck.java
@@ -0,0 +1,13 @@
+package _06_duck;
+
+public class duck {
+	String favoriteFood;
+	int numberOfFriends;
+
+	 duck(String favoriteFood, int numberOfFriends) {
+	       this.favoriteFood = favoriteFood;
+	       this.numberOfFriends = numberOfFriends;
+	}
+
+
+}
---
+++ src/_06_duck/duckrunner.java
@@ -0,0 +1,10 @@
+package _06_duck;
+
+public class duckrunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		duck duck = new duck ("harry",5);
+	}
+
+}
