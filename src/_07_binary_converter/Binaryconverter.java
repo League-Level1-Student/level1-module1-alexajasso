2a1525006cc16f6317e780608dde9efac63d745a
root <root@0cca7e69d50a>
2020-11-28 19:03:09 +0000

    Working on binary converter.

---
+++ src/_07_binary_converter/Binaryconverter.java
@@ -0,0 +1,33 @@
+package _07_binary_converter;
+
+import javax.swing.JOptionPane;
+
+public class Binaryconverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String result = convert("10011001"); 
+System.out.println(result);
+	}
+	static String convert(String input) {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+	
+}
